167e8403cc9ecdc8c74a162f991825d8399023f6
Kristian Orozco <korozco@lean-tech.io>
2021-09-25 20:48:39 -0500

    dashboard

--- resources/views/livewire/dashboard.blade.php
+++ resources/views/livewire/dashboard.blade.php
@@ -1,8 +1,9 @@
 <div>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
+            {{ __('Dashboard') }}  |   <a class="mr-3" href="{{route('lider.votaciones.diplomado')}}">    DIPLOMADO    </a>   |  <a class="mr-3" href="{{route('lider.votaciones.yahir')}}">     YAHIR     </a>
         </h2>
+
     </x-slot>
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
